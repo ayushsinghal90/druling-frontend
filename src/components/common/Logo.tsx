3bbda0543afa24530b7161ef4dce4c5c4acc5853
Ayush Singhal <ayushsinghal998@gmail.com>
2025-01-01 04:07:46 +0530

    update logo behaviour

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LogoImg from "./LogoImg";
+import { useNavigate } from "react-router-dom";
 
 const Logo = ({
   size = "h-8 w-8",
@@ -10,8 +11,12 @@ const Logo = ({
   color?: string;
   textSize?: string;
 }) => {
+  const navigate = useNavigate();
   return (
-    <div className="w-[140px] flex items-center">
+    <div
+      className="w-[140px] flex items-center cursor-default"
+      onClick={() => navigate("/")}
+    >
       <LogoImg className={`${size} ${color}`} />
       <span className={`${textSize} font-bold ${color} font-comfortaa p-2`}>
         druling
