44e02c7e73ac6f60b7675cb85d704639611042ce
Ayush Singhal <ayushsinghal998@gmail.com>
2025-02-09 23:02:22 +0530

    restrict

--- src/hooks/useProfileFeature.ts
+++ src/hooks/useProfileFeature.ts
@@ -1,43 +1,43 @@
 import { useGetAllFeaturesQuery } from "../store/services/featureApi";
-import { useDispatch } from "react-redux";
-import { setProfileFeatures } from "../store/slices/authSlice";
+// import { useDispatch } from "react-redux";
+// import { setProfileFeatures } from "../store/slices/authSlice";
 import { toast } from "react-toastify";
 import { ProfileFeature } from "../types";
 import { getStoredProfileFeatures } from "../utils/auth";
 
 export const useProfileFeature = () => {
   const {data: profileFeatureData} = useGetAllFeaturesQuery();
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   const setProfileFeature = async () => {
     try {
       if (profileFeatureData?.success) {
         const data: ProfileFeature[] = profileFeatureData?.data;
-        dispatch(
-            setProfileFeatures(data)
-          );
+        // dispatch(
+        //     setProfileFeatures(data)
+        //   );
+        return data;
       }
     } catch {
       toast.error("Profile feature update Failed");
     }
   };
 
-  const getProfileFeatures = async (): Promise<ProfileFeature[] | null> => { 
-    let features = getStoredProfileFeatures();
+  const getProfileFeatures = async (): Promise<ProfileFeature[] | undefined> => { 
+    // let features = getStoredProfileFeatures();
 
-    if (!features) {
-      await setProfileFeature();
-    }
+    // if (!features) {
+      
+    // }
 
-    features = getStoredProfileFeatures();
-    return features;
+    // features = getStoredProfileFeatures();
+    return await setProfileFeature();;
   };
 
   const checkFeature = async (featureType: string): Promise<boolean> => {
     const features = await getProfileFeatures();
     if (features) {
       const feature = features.find((f) => f.type === featureType);
-      console.log("checkFeature", feature);
 
       return feature ? feature.is_active && feature.usage < feature.limit : false;
     }
